5147eb14a9cb277a93366548f995aa37109f2cae
Chandragupt <chandragupt.mehata@lirisoft.com>
2022-06-02 13:02:35 +0530

    card color changes

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,8 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
-        backgroundColor: Colors.grey
+        backgroundColor: Colors.grey,
+        cardColor: Colors.red
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
